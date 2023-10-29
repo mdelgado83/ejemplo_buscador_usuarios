3131f62b45f7f48fcca1cbac568d60911b6c4d8a
Manuel Delgado <moroscity@gmail.com>
2023-10-29 18:20:27 +0100

    PASO 2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import { useState } from 'react';
 import './App.css';
 import Header from './Header';
+import Resultados from './Resultatos';
+import { mock1 } from './constants/users';
 
 function App() {
   const [query, setQuery] = useState("");
+  const [resultado,setResultado]=useState(null);
+
   const callServer = function () {
     console.log("CLIK");
+    setResultado(mock1.users);
   }
   return (
-    <div className="App">
+    <div  id="main">
       <Header />
       <h2 id="buscador">Buscador de usuario</h2>
       <input type='text' value={query} onChange={e => setQuery(e.target.value)} />
       <button id="butonsearch" onClick={() => callServer()}>Buscar</button>
+      {resultado && <Resultados resultado={resultado}/>}
     </div>
   );
 }
