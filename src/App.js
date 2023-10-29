21c303968e4ceb36a68a5a99de3d104509c8cb7b
Manuel Delgado Morte <60611222+mdelgado83@users.noreply.github.com>
2023-10-29 19:24:41 +0100

    PASO 3

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import { useState } from 'react';
-import './App.css';
-import Header from './Header';
-import Resultados from './Resultatos';
-import { mock1 } from './constants/users';
+import { useState } from "react";
+import "./App.css";
+import Header from "./Header";
+import Resultados from "./Resultatos";
+import { mock1 } from "./constants/users";
+import CONFIG from "./config/config";
 
 function App() {
   const [query, setQuery] = useState("");
-  const [resultado,setResultado]=useState(null);
+  const [resultado, setResultado] = useState(null);
 
-  const callServer = function () {
+  const callServer = async function () {
     console.log("CLIK");
-    setResultado(mock1.users);
-  }
+    if (CONFIG.use_server) {
+      const response = await fetch(CONFIG.server_url);
+      const data = await response.json();
+      console.log(data);
+      setResultado(data.users);
+    } else {
+      setResultado(mock1.users);
+    }
+  };
   return (
-    <div  id="main">
+    <div id="main">
       <Header />
       <h2 id="buscador">Buscador de usuario</h2>
-      <input type='text' value={query} onChange={e => setQuery(e.target.value)} />
-      <button id="butonsearch" onClick={() => callServer()}>Buscar</button>
-      {resultado && <Resultados resultado={resultado}/>}
+      <input
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <button id="butonsearch" onClick={() => callServer()}>
+        Buscar
+      </button>
+      {resultado && <Resultados resultado={resultado} />}
     </div>
   );
 }
